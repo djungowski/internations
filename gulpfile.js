612dd1bf50778e7722ab3ec709f72979879cd2bb
Dominik Ehrenberg <Dominik@Jungowski.de>
2017-01-22 18:58:29 +0100

    Run lib files through Babel

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,31 @@
 const gulp = require('gulp');
+const babel = require('gulp-babel');
 
 const buildDirectory = 'build';
+const assetsDirectory = buildDirectory + '/assets';
 
-gulp.task('vendor-assets', () => {
+gulp.task('vendor-assets-js', () => {
 	const webFiles = [
 		'node_modules/react/dist/react.min.js',
 		'node_modules/react-dom/dist/react-dom.min.js'
 	];
 
-	gulp.src(webFiles)
-		.pipe(gulp.dest(buildDirectory + '/assets/js'));
+	return gulp.src(webFiles)
+		.pipe(gulp.dest(assetsDirectory + '/js/vendor'));
+});
+
+gulp.task('lib-js', () => {
+	return gulp.src('lib/**/*').pipe(babel({
+		plugins: [
+			'transform-react-jsx'
+		]
+	})).pipe(gulp.dest(assetsDirectory + '/js/lib/'));
 });
 
 gulp.task('templates', () => {
-	gulp.src('templates/**/*').pipe(gulp.dest(buildDirectory));
+	return gulp.src('templates/**/*').pipe(gulp.dest(buildDirectory));
 });
 
-gulp.task('default', ['vendor-assets', 'templates']);
+gulp.task('build', ['vendor-assets-js', 'templates', 'lib-js']);
+
+gulp.task('default', ['build']);
