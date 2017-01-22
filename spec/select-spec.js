5be1a1d7e400ab1515660b3e3e92be31e790724f
Dominik Ehrenberg <Dominik@Jungowski.de>
2017-01-22 22:25:07 +0100

    Remove unused lines

--- spec/select-spec.js
+++ spec/select-spec.js
@@ -1,8 +1,8 @@
 //const ReactTestUtils = require('react-addons-test-utils');
 //import ReactTestUtils from 'react-addons-test-utils'
+
+// This is undefined. Yay.
 const ReactTestUtils = React.addons.TestUtils;
-console.log(React.addons);
-console.log(ReactTestUtils);
 const renderer = TestUtils.createRenderer();
 
 describe('Select component tests', function() {
