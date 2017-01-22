f81518d62775a93c3ab7277195ee825a0dcd8f9f
Dominik Ehrenberg <Dominik@Jungowski.de>
2017-01-23 00:35:17 +0100

    Tests for setSelectedUsers and setSelectedGroups

--- spec/user-group-spec.js
+++ spec/user-group-spec.js
@@ -12,4 +12,39 @@ describe('User group component tests', function() {
 		const result = this.renderer.getRenderOutput();
 		expect(result.type).toBe('form');
     });
+
+	it('has a default state', function() {
+	    const userGroup = ReactTestUtils.renderIntoDocument(<UserGroup />);
+		expect(userGroup.state).toEqual({
+			selectedUsers: [],
+			selectedGroups: []
+		});
+	});
+
+	describe('#setSelectedUsers()', function() {
+	    it('alters the state', function() {
+			const selectedUsers = {};
+			const userGroup = ReactTestUtils.renderIntoDocument(<UserGroup />);
+			userGroup.setSelectedUsers(selectedUsers);
+			expect(userGroup.state.selectedUsers).toBe(selectedUsers);
+	    });
+	});
+
+	describe('#setSelectedGroups()', function() {
+		it('alters the state', function() {
+			const selectedGroups = {};
+			const userGroup = ReactTestUtils.renderIntoDocument(<UserGroup />);
+			userGroup.setSelectedGroups(selectedGroups);
+			expect(userGroup.state.selectedGroups).toBe(selectedGroups);
+		});
+	});
+
+	describe('#addUserToGroup', function() {
+	    it('prevents the default', function() {
+	        const eventMock = jasmine.createSpyObj('event', ['preventDefault']);
+			const userGroup = ReactTestUtils.renderIntoDocument(<UserGroup />);
+			userGroup.addUserToGroup(eventMock);
+			expect(eventMock.preventDefault).toHaveBeenCalled();
+	    });
+	});
 });
