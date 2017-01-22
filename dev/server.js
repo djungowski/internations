81cfbeb10a0a5f1d8cb98aae9125cb8d8fe697d0
Dominik Ehrenberg <Dominik@Jungowski.de>
2017-01-22 17:49:38 +0100

    Dev server

---
+++ dev/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || '8080';
+
+app.use('/', express.static(__dirname + '/../build/'));
+
+app.listen(port, function () {
+	console.log('Dev server running on port ' + port);
+});
