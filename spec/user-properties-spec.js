07724134e2635e9155436f325568bcea70d004e9
Dominik Ehrenberg <Dominik@Jungowski.de>
2017-01-23 07:44:06 +0100

    User properties box

---
+++ spec/user-properties-spec.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactTestUtils from 'react-addons-test-utils';
+import UserProperties from '../lib/user-properties'
+
+describe('UserProperties component tests', function() {
+	let shallowRenderer;
+
+	beforeEach(function() {
+	    shallowRenderer = ReactTestUtils.createRenderer();
+	});
+
+    it('can be rendered without a user', function() {
+		shallowRenderer.render(<UserProperties />);
+		const result = shallowRenderer.getRenderOutput();
+		expect(result.type).toEqual('div');
+    });
+
+	it('shows the user name and id', function() {
+		const user = {id: 5, name: 'Dr. Zoidberg'}
+		shallowRenderer.render(<UserProperties user={user} />);
+		const result = shallowRenderer.getRenderOutput();
+		expect(result.props.children[0]).toEqual(<p class="user-id">{user.id}</p>);
+		expect(result.props.children[1]).toEqual(<p class="user-name">{user.name}</p>);
+	});
+});
