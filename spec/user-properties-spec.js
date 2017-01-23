ba4519b4f27ea59feb5ff712bc215518ab69b989
Dominik Ehrenberg <Dominik@Jungowski.de>
2017-01-23 08:13:56 +0100

    Show groups

--- spec/user-properties-spec.js
+++ spec/user-properties-spec.js
@@ -15,11 +15,13 @@ describe('UserProperties component tests', function() {
 		expect(result.type).toEqual('div');
     });
 
-	it('shows the user name and id', function() {
-		const user = {id: 5, name: 'Dr. Zoidberg'}
+	it('shows the user name, id and groups', function() {
+		const groups = [{id: 1, name: 'Futurama'}, {id: 2, name: 'Animated'}];
+		const user = {id: 5, name: 'Dr. Zoidberg', groups: groups}
 		shallowRenderer.render(<UserProperties user={user} />);
 		const result = shallowRenderer.getRenderOutput();
 		expect(result.props.children[0]).toEqual(<p className="user-id">ID: {user.id}</p>);
 		expect(result.props.children[1]).toEqual(<p className="user-name">Name: {user.name}</p>);
+		expect(result.props.children[2]).toEqual(<p className="user-groups">Groups: Futurama, Animated</p>);
 	});
 });
